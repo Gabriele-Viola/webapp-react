130d947c57465e8d0bcdeff21b00ff14ee4badaf
Gabriele-Viola <gabrieleviola50@gmail.com>
2024-12-11 14:56:28 +0100

    add reviewsCard components

---
+++ src/components/ReviewCard.jsx
@@ -0,0 +1,13 @@
+export default function ReviewCard() {
+    return (
+        <div className="review card mb-3">
+            <div className="card-body">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, tenetur?</p>
+                <span>by: user</span>
+                <div className="vote mt-3">
+                    <strong>Vote: 4/5</strong>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/pages/FilmSelected.jsx
+++ src/pages/FilmSelected.jsx
@@ -1,19 +1,16 @@
 import { useParams } from "react-router"
+import ReviewCard from "../components/ReviewCard"
 export default function FilmSelected() {
     const { id } = useParams()
     return (
         <>
             <section className="reviews">
                 <div className="container">
-                    <div className="review card mb-3">
-                        <div className="card-body">
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, tenetur?</p>
-                            <span>by: user</span>
-                            <div className="vote mt-3">
-                                <strong>Vote: 4/5</strong>
-                            </div>
-                        </div>
-                    </div>
+                    <ReviewCard />
+                    <ReviewCard />
+                    <ReviewCard />
+                    <ReviewCard />
+
                 </div>
             </section>
 
