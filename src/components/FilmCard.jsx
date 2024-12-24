0fb2b778f7aa609d28c239e83908ad20cd443cb2
Gabriele-Viola <gabrieleviola50@gmail.com>
2024-12-24 01:10:16 +0100

    fix layout card

--- src/components/FilmCard.jsx
+++ src/components/FilmCard.jsx
@@ -2,14 +2,16 @@ import { Link } from "react-router";
 
 export default function FilmCard({ film }) {
     return (
-        <div className="card h-100">
-            <div className="card-body">
+        <div className="card h-100 ">
+            <div className="card-body d-flex flex-column justify-content-between">
                 <h3>{film.title}</h3>
                 <h5>{film.genre}</h5>
                 <p>{film.abstract}</p>
                 <div className="mb-1">Direct by: {film.director}</div>
                 <div className="mb-2">realese: {film.release_year}</div>
-                <Link to={`/films/${film.id}`} className="btn btn-secondary" >Read More</Link>
+                <div>
+                    <Link to={`/films/${film.id}`} className="btn btn-secondary" >Read More</Link>
+                </div>
             </div>
         </div>
     )
